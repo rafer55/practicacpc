2d098fc99416a2d5fccb9abdee1d3e9d790a54f6
rafer55 <raul.f.arana@gmail.com>
2019-10-06 09:55:20 -0600

    practica en cpc

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,20 +6,24 @@
     </div>
     <form action="" method="post">
     <div class="row">
-        <div class="col-sm-4 col-md-offset-4" style="padding-bottom: 10px">
-            <div class="col-sm-6"><label for="nombre">Ingresar nombre</label></div>
+        <div class="col-sm-8 col-md-offset-4" style="padding-bottom: 10px">
+            <div class="col-sm-3"><label for="nombre">Ingresar nombre</label></div>
             <div class="col-sm-6"><input type="text" name="nombre" id="nombre"></div>
         </div>
-        <div class="col-sm-4 col-md-offset-4" style="padding-bottom: 10px">
-            <div class="col-sm-6"><label for="email">Ingresar email</label></div>
+        <div class="col-sm-8 col-md-offset-4" style="padding-bottom: 10px">
+            <div class="col-sm-3"><label for="email">Ingresar email</label></div>
             <div class="col-sm-6"><input type="email" name="email" id="email"></div>
         </div>
-        <div class="col-sm-4 col-md-offset-4" style="padding-bottom: 10px">
-            <div class="col-sm-6"><label for="password">Contraseña</label></div>
-            <div class="col-sm-6"><input type="password" name="password" id="password"></div>
+        <div class="col-sm-8 col-md-offset-4" style="padding-bottom: 10px">
+            <div class="col-sm-3"><label for="asunto">Asunto</label></div>
+            <div class="col-sm-6"><input type="asunto" name="asunto" id="asunto"></div>
+        </div>
+        <div class="col-sm-8 col-md-offset-4" style="padding-bottom: 10px">
+            <div class="col-sm-3"><label for="mensaje">Mensaje</label></div>
+            <div class="col-sm-6"><textarea name="mensaje" id="mensaje" cols="40" rows="5"></textarea></div>
         </div>
         <div class="col-sm-4 col-md-offset-6" style="padding-bottom: 10px">
-            <input type="submit" value="Enviar">
+            <input type="submit" value="Enviar " class="btn-primary">
         </div>
     </div>
     </form>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,6 @@
 @section('tittle','Inicio')
 @section('seccionPpal')
 <div style="padding: 20px; text-align: center">
-    <h1>Pagina de Inicio</h1>
+    <h1>Pagina de Inicio </h1>
     </div>
 @endsection
