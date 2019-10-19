a50f5b7df05f495262e1f311efa0bad7e3deb325
unknown <raul.f.arana@gmail.com>
2019-10-19 15:38:30 -0600

    actualizacion de bootstrap 4

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,23 +7,23 @@
     <form action="{{ route('contactpost') }}" method="post"> 
         @csrf
     <div class="row">
-        <div class="col-sm-8 col-md-offset-4" style="padding-bottom: 10px">
+        <div class="col-sm-8 offset-4" style="padding-bottom: 10px">
             <div class="col-sm-3"><label for="nombre">Ingresar nombre</label></div>
             <div class="col-sm-6"><input type="text" name="nombre" id="nombre"> </div>
         </div>
-        <div class="col-sm-8 col-md-offset-4" style="padding-bottom: 10px">
+        <div class="col-sm-8 offset-4" style="padding-bottom: 10px">
             <div class="col-sm-3"><label for="email">Ingresar email</label></div>
             <div class="col-sm-6"><input type="email" name="email" id="email"> </div>
         </div>
-        <div class="col-sm-8 col-md-offset-4" style="padding-bottom: 10px">
+        <div class="col-sm-8 offset-4" style="padding-bottom: 10px">
             <div class="col-sm-3"><label for="asunto">Asunto</label></div>
             <div class="col-sm-6"><input type="asunto" name="asunto" id="asunto"> </div>
         </div>
-        <div class="col-sm-8 col-md-offset-4" style="padding-bottom: 10px">
+        <div class="col-sm-8 offset-4" style="padding-bottom: 10px">
             <div class="col-sm-3"><label for="mensaje">Mensaje</label></div>
             <div class="col-sm-6"><textarea name="mensaje" id="mensaje" cols="40" rows="5"></textarea> </div>
         </div>
-        <div class="col-sm-4 col-md-offset-6" style="padding-bottom: 10px">
+        <div class="col-sm-4 offset-6" style="padding-bottom: 10px">
             <input type="submit" value="Enviar" class="btn-primary">
         </div>
     </div>
--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -5,13 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel @yield('tittle') </title>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
+    
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
