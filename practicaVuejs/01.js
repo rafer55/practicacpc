5d3808a4c076f586bf452db0fa6121621c4a7fad
rafer55 <raul.f.arana@gmail.com>
2019-11-24 11:20:24 -0600

    vue2

--- practicaVuejs/01.js
+++ practicaVuejs/01.js
@@ -3,15 +3,30 @@ const app = new Vue({
     data:{
         titulo: "mi primer componente de vue",
         actividades:[],
-        nuevaActividad:''
+        nuevaActividad:'',
+        actividadActual:''
     },
     methods:{
         agregarActividad: function(){
             this.actividades.push(this.nuevaActividad);
             this.nuevaActividad='';
+            localStorage.setItem('actividades',JSON.stringify(this.actividades));
         },
         eliminarActividad: function(index){
-            this.actividades.splice(index,1);
-        }
+           let datos = JSON.parse(localStorage.getItem('actividades'));
+           datos.splice(index,1);
+           localStorage.setItem('actividades',JSON.stringify(datos));
+           this.actividades.splice(index,1);
+        },
+        actualizarActividad: function(index){
+            let datos = JSON.parse(localStorage.getItem('actividades'));
+            this.actividadActual = datos[index];
+            //datos.splice(index,1);
+            //localStorage.setItem('actividades',JSON.stringify(datos));
+            //this.actividades.splice(index,1);
+         }
+    },
+    create: function(){
+        let datos = JSON.parse(localStorage.getItem('actividades'));        
     }
 });
